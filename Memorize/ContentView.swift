f490560f45f44cb20d65c9a1fb58cd9abb1ff9cb
Daniel Martinez Condinanza <daniel.martinezcg@gmail.com>
2020-05-21 23:08:39 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Daniel Martinez Condinanza on 5/21/20.
+//  Copyright © 2020 danielmartinezcg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
